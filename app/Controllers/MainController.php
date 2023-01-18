6e247cefb3055aa618a8b9f5f64d28fe04fc64a2
Sofiane Khenoune <modestman1986@yahoo.com>
2023-01-18 20:11:28 +0100

    Mise en place du MainController

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -2,7 +2,40 @@
 
 namespace Salary\Controllers;
 
+use Salary\Models\EmployeeModel;
+
 class MainController 
 {
-    
+    public function homeAction()
+    {
+        
+    }
+
+
+    public function testAction()
+    {
+        $employeeModel = new EmployeeModel();
+        $employees = $employeeModel->getAllEmployees(); 
+        $this->showViews('employees', [$employees]);
+    }
+   
+    public function showViews($viewName, $dataArray = [])
+    {
+        global $router;
+        $absoluteURL = $_SERVER['BASE_URI'];
+        require_once __DIR__ . '/../views/partials/_header.tpl.php';
+        require_once __DIR__ . '/../views/main/' . $viewName . '.tpl.php';
+        require_once __DIR__ . '/../views/partials/_footer.tpl.php';
+    }
+
+     /**
+     * Fonction qui retourne la page erreur en cas de route inconnue
+     *
+     * @return error
+     */
+    public function pageNotFound()
+    {
+        header("HTTP/1.1 404 Not Found");
+        $this->showViews('404');
+    }
 }
