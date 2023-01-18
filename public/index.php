1f40a1e25a94cedb1a17506a2bc40fbfdc18c369
Sofiane Khenoune <modestman1986@yahoo.com>
2023-01-18 20:13:20 +0100

    Mise en place du fichier index avec une route test

--- public/index.php
+++ public/index.php
@@ -1,41 +1,46 @@
-<?php
+<?Php
 
-// Chargement des dépendances via autoload de composer:
+// Chargement des dépendances avec le fichier 'autoload' de php, permet de charger 
+// les controllers, les models, les classes utiles au code et les vues
 require __DIR__ . '/../vendor/autoload.php';
 
-// Préparation d'Altorouter
 $router = new AltoRouter();
 
-// Définition de la racine de notre projet (path racine)
-// Eviter absolument tout caractère spécial ou espace dans les noms des dossiers
-$router->setBasePath($_SERVER['BASE_URI']);
-
+// Route vers la page d'accueil
 //$router->map('GET','/',['controller' => 'MainController','method' => 'homeAction',],'home');
 
-//$router->map('GET','/catalogue/categorie/[i:id]',['controller' => 'CatalogController','method' => 'categoryAction',], 'category' );
+// Route vers la page des créateur de Sonic
+//$router->map('GET','/createurs',['controller' => 'MainController','method' => 'creatorsAction',],'creators');
 
-//$router->map('GET','/mentions-legales',['controller' => 'MainController','method' => 'legalNoticeAction',],'legal-notice');
+// Route vers la page des créateur de Sonic
+//$router->map('GET','/personnage/[i:id]',['controller' => 'MainController','method' => 'characterAction',],'character');
 
-// renvie un tableau avec plusieurs avec les index : target, method, params
+// Route vers la page de test des controllers et méthodes
+$router->map('GET','/test',['controller' => 'MainController','method' => 'testAction',],'test');
+
+// Définition de la racine de notre projet
+$router->setBasePath($_SERVER['BASE_URI']);
+
+// La méthode match renvoie un tableau
 $match = $router->match();
 
 if ($match !== false) {
-    // On récupère le tableau correspondant à la route recherchée
-    $routeInfos = $match['target'];
-  
-    // On récupère dans ce tableau le contrôleur à utiliser et la méthode à appeler
-    $controllerToUse = '\\Oshop\\Controllers\\' . $routeInfos['controller'];
-    $method = $routeInfos['method'];
-    $urlParams = $match['params'];
-  
-    // On crée une nouvelle instance de notre contrôleur qu'on stocke dans une variable (C'est un objet)
-    $controller = new $controllerToUse();
-    // $controller = new MainController();
-  
-    // On prend l'objet contrôleur créé et on on exécute la méthode dessus
-    $controller->$method($urlParams);
-    // $controller->homeAction();
-  } else {
-    $controller = new \Salary\Controllers\MainController();
-    //$controller->pageNotFound();
+  // On récupère le tableau correspondant à la route recherchée
+  $routeInfos = $match['target'];
+
+  // On récupère dans ce tableau le contrôleur à utiliser et la méthode à appeler
+  $controllerToUse = '\\Salary\\Controllers\\' . $routeInfos['controller'];
+  $method = $routeInfos['method'];
+  $params = $match['params'];
+
+  // On crée une nouvelle instance de notre contrôleur qu'on stocke dans une variable (C'est un objet)
+  $controller = new $controllerToUse();
+  // $controller = new MainController();
+
+  // On prend l'objet contrôleur créé et on on exécute la méthode dessus
+  $controller->$method($params);
+  // $controller->homeAction();
+} else {
+  $controller = new \Salary\Controllers\MainController();
+  $controller->pageNotFound();
 }
